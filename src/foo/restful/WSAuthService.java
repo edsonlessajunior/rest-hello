113b9f0cf4449945fdba07c52e56fde14d85ac11
Edson Lessa <edsonlessajunior@gmail.com>
2017-11-23 20:47:51 -0200

    primeiro

---
+++ src/foo/restful/WSAuthService.java
@@ -0,0 +1,16 @@
+package foo.restful;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("service")
+public class WSAuthService {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello(){
+		return "Hello World";
+	}
+}
